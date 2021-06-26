c98f1fc54f862a1473acb142539a3b59fefdf528
Taheir Ahmed Ashraf <programmerashraf001@gmail.com>
2021-06-26 16:43:41 +0600

    Greeting App React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const date = new Date()
+const hour = date.getHours()
+// const hour = 19
+const style = {}
+var greeting;
+
+if(hour >= 1 && hour < 12){
+  var greeting = "Good Morning"
+  style.color = "green"
+}else if(hour >= 12 && hour < 19){
+  var greeting = "Good Afternoon"
+  style.color = "orange"
+}else{
+  var greeting = "Good Night"
+  style.color = "black"
+}
+
+
+ReactDOM.render(
+  <h1>Hello Sir! <span style={style}>{greeting}</span></h1>,
+  document.getElementById('root'));
+
+
+  reportWebVitals();
+  
+
+
+
+
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
